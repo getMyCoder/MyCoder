2eab4f6577c5be88a4118f25ca3b578532aef01b
getMyCoder <1598719304@qq.com>
2018-12-07 10:39:53 +0800

    add

---
+++ getAdd.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type:text/html;charset=utf-8');
+$url = $_GET["user"];
+$title = $_GET["title"];
+$href = $_GET["href"];
+if ($url && $title && $href) {
+    $Reads = file_get_contents("json.json");
+    $SetReads = json_decode($Reads, true);
+    $ArrLength = count($SetReads);
+    $SetReads[$ArrLength]["url"] = $url;
+    $SetReads[$ArrLength]["title"] = $title;
+    $SetReads[$ArrLength]["href"] = $href;
+    $SetW = json_encode($SetReads);
+    file_put_contents("json.json", $SetW);
+    echo "https://getmycoder.github.io/MyCoder/";
+}else{
+    echo "error";
+}
+?>
